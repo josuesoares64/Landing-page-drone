5e483208e6e1801b8cc582fa298cb0a9ee285810
Josue Soares <josuesoares318@gmail.com>
2025-10-15 11:08:31 -0300

    Animações no hero

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
   menuToggle.classList.toggle("open");
 });
+
+ document.addEventListener("DOMContentLoaded", function() {
+      AOS.init({
+        duration: 800,
+        once: true,
+        offset: 100,
+        easing: 'ease-out',
+      });
+    });
